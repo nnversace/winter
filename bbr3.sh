35c0cc04ac5f7990d8756d1b83db9789c1cea341
nnversace <166464333+nnversace@users.noreply.github.com>
2024-06-03 09:26:37 +0800

    Update bbr3.sh

--- bbr3.sh
+++ bbr3.sh
@@ -6,12 +6,20 @@ wget -q --show-progress $(wget -q -O - https://api.github.com/repos/love4taylor/
 # 安裝下載的 .deb 文件
 sudo dpkg -i linux-headers-*-egoist-cloud_*.deb && sudo dpkg -i linux-image-*-egoist-cloud_*.deb
 
-# 設置 TCP 擁塞控制算法為 BBR
+# 設置 TCP 擁塞控制算法為 BBR，啟用 FQ 和 ECN
 echo 'net.ipv4.tcp_congestion_control=bbr' | sudo tee -a /etc/sysctl.conf
+echo 'net.core.default_qdisc=fq' | sudo tee -a /etc/sysctl.conf
+echo 'net.ipv4.tcp_ecn=1' | sudo tee -a /etc/sysctl.conf
 sudo sysctl -p
 
 # 顯示當前 TCP 擁塞控制算法
 sysctl net.ipv4.tcp_congestion_control
+
+# 顯示當前的隊列規則
+sysctl net.core.default_qdisc
+
+# 顯示 ECN 設置
+sysctl net.ipv4.tcp_ecn
 
 # 顯示可用的 TCP 擁塞控制算法
 sysctl net.ipv4.tcp_available_congestion_control
