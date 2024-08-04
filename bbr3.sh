048124fa3799bb97e8e91b60ed404c0e2c511b48
nnversace <166464333+nnversace@users.noreply.github.com>
2024-08-04 20:20:34 +0800

    Update bbr3.sh

--- bbr3.sh
+++ bbr3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# 更新軟件包列表
+# 更新软件包列表
 sudo apt-get update
 
 # 检测并安装缺失的软件包
@@ -9,8 +9,7 @@ REQUIRED_PKG=("wget" "jq")
 for PKG in "${REQUIRED_PKG[@]}"; do
     if ! dpkg -l | grep -q "$PKG"; then
         echo "$PKG is not installed. Installing..."
-        sudo apt update
-        sudo apt install -y "$PKG"
+        sudo apt-get install -y "$PKG"
     else
         echo "$PKG is already installed."
     fi
@@ -34,32 +33,32 @@ sudo dpkg -i linux-headers-*-egoist-cloud_*.deb && sudo dpkg -i linux-image-*-eg
 echo "Cleaning up downloaded files..."
 rm -f linux-headers-*-egoist-cloud_*.deb linux-image-*-egoist-cloud_*.deb
 
-# 修改 /etc/sysctl.conf 文件
-echo "Modifying /etc/sysctl.conf..."
-sudo sed -i '/net.ipv4.tcp_no_metrics_save/d' /etc/sysctl.conf
-sudo sed -i '/net.ipv4.tcp_ecn/d' /etc/sysctl.conf
-sudo sed -i '/net.ipv4.tcp_frto/d' /etc/sysctl.conf
-sudo sed -i '/net.ipv4.tcp_mtu_probing/d' /etc/sysctl.conf
-sudo sed -i '/net.ipv4.tcp_rfc1337/d' /etc/sysctl.conf
-sudo sed -i '/net.ipv4.tcp_sack/d' /etc/sysctl.conf
-sudo sed -i '/net.ipv4.tcp_fack/d' /etc/sysctl.conf
-sudo sed -i '/net.ipv4.tcp_window_scaling/d' /etc/sysctl.conf
-sudo sed -i '/net.ipv4.tcp_adv_win_scale/d' /etc/sysctl.conf
-sudo sed -i '/net.ipv4.tcp_moderate_rcvbuf/d' /etc/sysctl.conf
-sudo sed -i '/net.ipv4.tcp_rmem/d' /etc/sysctl.conf
-sudo sed -i '/net.ipv4.tcp_wmem/d' /etc/sysctl.conf
-sudo sed -i '/net.core.rmem_max/d' /etc/sysctl.conf
-sudo sed -i '/net.core.wmem_max/d' /etc/sysctl.conf
-sudo sed -i '/net.ipv4.udp_rmem_min/d' /etc/sysctl.conf
-sudo sed -i '/net.ipv4.udp_wmem_min/d' /etc/sysctl.conf
-sudo sed -i '/net.core.default_qdisc/d' /etc/sysctl.conf
-sudo sed -i '/net.ipv4.tcp_congestion_control/d' /etc/sysctl.conf
-sudo sed -i '/net.ipv4.conf.all.route_localnet/d' /etc/sysctl.conf
-sudo sed -i '/net.ipv4.ip_forward/d' /etc/sysctl.conf
-sudo sed -i '/net.ipv4.conf.all.forwarding/d' /etc/sysctl.conf
-sudo sed -i '/net.ipv4.conf.default.forwarding/d' /etc/sysctl.conf
+# 删除旧配置
+sed -i '/net.ipv4.tcp_no_metrics_save/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_ecn/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_frto/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_mtu_probing/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_rfc1337/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_sack/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_fack/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_window_scaling/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_adv_win_scale/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_moderate_rcvbuf/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_rmem/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_wmem/d' /etc/sysctl.conf
+sed -i '/net.core.rmem_max/d' /etc/sysctl.conf
+sed -i '/net.core.wmem_max/d' /etc/sysctl.conf
+sed -i '/net.ipv4.udp_rmem_min/d' /etc/sysctl.conf
+sed -i '/net.ipv4.udp_wmem_min/d' /etc/sysctl.conf
+sed -i '/net.core.default_qdisc/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_congestion_control/d' /etc/sysctl.conf
+sed -i '/net.ipv4.conf.all.route_localnet/d' /etc/sysctl.conf
+sed -i '/net.ipv4.ip_forward/d' /etc/sysctl.conf
+sed -i '/net.ipv4.conf.all.forwarding/d' /etc/sysctl.conf
+sed -i '/net.ipv4.conf.default.forwarding/d' /etc/sysctl.conf
 
-sudo bash -c 'cat >> /etc/sysctl.conf << EOF
+# 添加新配置
+cat >> /etc/sysctl.conf << EOF
 net.ipv4.tcp_no_metrics_save=1
 net.ipv4.tcp_ecn=0
 net.ipv4.tcp_frto=0
@@ -82,10 +81,13 @@ net.ipv4.conf.all.route_localnet=1
 net.ipv4.ip_forward=1
 net.ipv4.conf.all.forwarding=1
 net.ipv4.conf.default.forwarding=1
-EOF'
+net.ipv6.conf.all.disable_ipv6=1
+net.ipv6.conf.default.disable_ipv6=1
+net.ipv6.conf.lo.disable_ipv6=1
+EOF
 
-# 应用 sysctl 配置
-echo "Applying sysctl settings..."
+# 应用新配置
+echo "Applying new configuration..."
 sudo sysctl -p && sudo sysctl --system
 
-echo "Script execution completed."
+echo "Script execution completed successfully."
